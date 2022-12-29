21f442adc8837751f828198731ee6cf6819b3b50
Abdelkader Bouzomita <abdelkaderbouzmita@takiacademyteam.com>
2022-12-29 10:21:01 +0100

    fix some bugs in login js file

--- jsFile/login.js
+++ jsFile/login.js
@@ -4,29 +4,50 @@ const confirmPw = document.querySelector(".confirmPass--input");
 const emailError = document.querySelector(".err--email");
 const passWordError = document.querySelector(".err--pass");
 const confirmPassWordError = document.querySelector(".err--confipass");
-const loginBtn=document.querySelector(".login--btn")
+const loginLink=document.querySelector(".login--link")
 const resetBtn = document.querySelector(".reset--btn")
 
 
 
 
-
-loginBtn.addEventListener("click", function (e)
+loginLink.addEventListener("click", function (e)
 {
-  e.preventDefault()
-  if (email.value==false)
+  let valid = true;
+  if (
+    email.value.substring(email.value.indexOf("@")) !== "@gmail.com" ||
+    email.value.substring(0, email.value.indexOf("@")).length < 4
+  )
   {
-    e.preventDefault()
-    emailError.textContent = "Email Non Valid!!";
-    return false;
-  }else if ((passWord.value).lenght < 8)
+    valid = false;
+
+    emailError.textContent = "email invalid!!";
+    email.classList.add("back--red");
+  } else
   {
-    passWordError.textContent = "password invalid!!";
-    return false;
-  }else if (confirmPw.value !== passWord.value)
-  {
-    confirmPassWordError.textContent = "confirm password invalid!!";
-    return false;
+    emailError.textContent = "";
+    email.classList.remove("back--red");
   }
- 
-})
+  if (passWord.value.length < 8)
+  {
+    valid = false;
+
+    passWordError.textContent = "password invalid!!";
+    passWord.classList.add("back--red");
+  } else
+  {
+    passWordError.textContent = "";
+    passWord.classList.remove("back--red");
+  }
+  if (confirmPw.value.trim() === "" || confirmPw.value !== passWord.value)
+  {
+    valid = false;
+
+    confirmPassWordError.textContent = "password invalid!!";
+    confirmPw.classList.add("back--red");
+  } else
+  {
+    confirmPassWordError.textContent = "";
+    confirmPw.classList.remove("back--red");
+  }
+});
+
