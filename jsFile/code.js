9498549beb2a61bf68d8cb3003e6c7156e7de970
Abdelkader Bouzomita <abdelkaderbouzmita@takiacademyteam.com>
2022-12-29 21:52:28 +0100

    add some function in home page

--- jsFile/code.js
+++ jsFile/code.js
@@ -9,6 +9,8 @@ const arrowAyah = document.querySelector(".arrow--sort--ayahs");
 const arrowNumber = document.querySelector(".arrow--sort--number");
 const arrowAlphabes = document.querySelector(".arrow--sort--alphabes");
 const allArrowSort = document.querySelectorAll(".arrow");
+const middle = document.querySelector(".middle--container");
+const sortedBar = document.querySelector(".sorted--bar");
 console.log(arrowAlphabes);
 console.log(arrowAyah);
 nameProfile.textContent = window.localStorage.namePerson;
@@ -86,7 +88,8 @@ const alldata = fetch(`https://apitest.khouaja.live./v1/quran`, {
         apendchild(ele);
       });
     });
-
+    
+    let printaya = [];
     const allSourahContainer = document.querySelectorAll(".sourah--container");
     console.log(allSourahContainer);
     allSourahContainer.forEach((el) =>
@@ -100,17 +103,29 @@ const alldata = fetch(`https://apitest.khouaja.live./v1/quran`, {
 
             headers: {
               "Content-type": "application/json",
-
+              
               Authorization: `Bearer ${window.localStorage.getItem("token")}`,
             },
           }
-        )
+          )
           .then((res) => res.json())
-          .then((res) => console.log(res));
-      })
-    );
-  });
+          .then((res) =>
+          {
+            middle.textContent = "";
+            console.log(res.data.ayahs);
+            const [arrayOfAyah] = res.data.ayahs
+            let Str ="" ;
+            const allaya = res.data.ayahs.reverse().forEach((elem) => Str = elem.numberInSurah + " " + elem.text + Str);
+            console.log(Str)
+          }); 
+       
 
+        })
+        );
+        
+  });
+// .text
+// numberInSurah;
 function compare(a, b) {
   if (a.englishName < b.englishName) {
     return -1;
